b4b84d98041b6d8bebcb4a7d918402fbb3c926a3
M0RE4 <MagnusHDalgaard@gmail.com>
2023-03-09 13:22:42 +0100

    en test code

---
+++ Projekt.py
@@ -0,0 +1,18 @@
+import random
+
+def scramble_word(word):
+    if len(word) <= 2:
+        return word
+    else:
+        middle = list(word[1:-1])
+        random.shuffle(middle)
+        return word[0] + ''.join(middle) + word[-1]
+
+def scramble_file(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+        words = content.split()
+        scrambled_words = [scramble_word(word) for word in words]
+        scrambled_content = ' '.join(scrambled_words)
+    with open(filename, 'w') as file:
+        file.write(scrambled_content)
