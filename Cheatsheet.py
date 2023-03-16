cd39f635bd3521307ee958ce9a730056fe4720e8
M0RE4 <MagnusHDalgaard@gmail.com>
2023-03-16 11:57:24 +0100

    oversat

--- Cheatsheet.py
+++ Cheatsheet.py
@@ -1,25 +1,25 @@
 import random
 
-# Define a function that scrambles the middle letters of a word
+# Definer en funktion som scrambler bogstaverne i midten af et ord
 def scramble_word(word):
-    # If the word has only 2 or fewer characters, we can't scramble it
+    # Hvis ordet kun har 2 bogstaver, så kan det ikke scrambles
     if len(word) <= 2:
         return word
     else:
-        # Split the word into its first, middle, and last letters
+        # Split ordet op i start, midt, og sidst bogstav
         first, middle, last = word[0], word[1:-1], word[-1]
-        # Shuffle the middle letters randomly
+        # blande midten af bogstaverne random
         shuffled_middle = ''.join(random.sample(middle, len(middle)))
-        # Return the word with its middle letters shuffled
+        # Return ordet hvor den har blandet bogstaverne i midten
         return first + shuffled_middle + last
 
-# Define a function that scrambles the contents of a text file
+# Definer en funktion som blander indholdet i en text fil
 def scramble_file(filename):
-    # Open the file for reading and read its contents
+    # Åben filen for at læse indholdet
     with open(filename, 'r') as file:
         content = file.read()
-    # Split the contents into words, scramble each word, and join the scrambled words back into a string
+    # Split indholdet ind i ord, blande hvert ord, og samle de blandede ord tilbage til en String
     scrambled_content = ' '.join([scramble_word(word) for word in content.split()])
-    # Open the file for writing and write the scrambled contents
+    # Åben filen for at skrive de blandede ord
     with open(filename, 'w') as file:
         file.write(scrambled_content)
